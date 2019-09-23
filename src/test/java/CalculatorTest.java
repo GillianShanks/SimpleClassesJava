961d532c970072c0d73e73fb492c09ef2496dd8e
Gillian Shanks <GillianShanks88@gmail.com>
2019-09-23 16:00:24 +0100

    Initial commit - sets up Calculator class

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(10.0, calculator.add(4.0, 6.0), 0.01);
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(5.0, calculator.subtract(11.0, 6.0), 0.01);
+    }
+}
